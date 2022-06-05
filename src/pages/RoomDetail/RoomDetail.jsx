ebef3b01cbd0017252f84e758b275379f76df949
Link1515 <h26281660@gmail.com>
2022-06-05 16:23:59 +0800

    feat(roomdetail page): 建立房間細節頁

---
+++ src/pages/RoomDetail/RoomDetail.jsx
@@ -0,0 +1,13 @@
+import { useParams } from 'react-router-dom';
+import Card from '../../components/Card';
+
+export default function RoomDetail() {
+  const params = useParams();
+
+  return (
+    <div>
+      <h1>{params.roomId}</h1>
+      <Card />
+    </div>
+  );
+}
