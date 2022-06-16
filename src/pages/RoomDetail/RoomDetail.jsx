b35e8a49fbf4a0682d41327f3d220ecdfad604b8
Link1515 <h26281660@gmail.com>
2022-06-16 16:28:14 +0800

    feat(roomdetail.jsx): 加入 imgshower 組件

--- src/pages/RoomDetail/RoomDetail.jsx
+++ src/pages/RoomDetail/RoomDetail.jsx
@@ -1,11 +1,28 @@
+import { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
+// api
+import api from '@/api';
+// components
+import ImgShower from '@/components/ImgShower';
 
 export default function RoomDetail() {
+  const [roomInfo, setRoomInfo] = useState({});
+  const [bookInfo, setBookInfo] = useState({});
   const params = useParams();
+
+  const fetchRoomDetail = useCallback(async () => {
+    const { data } = await api.room.fetchRoomDetail(params.roomId);
+    setRoomInfo(data.room[0]);
+    setBookInfo(data.book);
+  }, [params]);
+
+  useEffect(() => {
+    fetchRoomDetail();
+  }, [fetchRoomDetail]);
 
   return (
     <div>
-      <h1>{params.roomId}</h1>
+      <ImgShower roomInfo={roomInfo} />
     </div>
   );
 }
