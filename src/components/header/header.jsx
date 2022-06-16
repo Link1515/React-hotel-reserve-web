71881ced6a472c1260b42fa133816aa71f720f98
Link1515 <h26281660@gmail.com>
2022-06-16 11:24:52 +0800

    feat(header.jsx): header logo 加入返回首頁連結功能

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <div className="flex justify-between bg-white">
-      <div className="px-8 py-3 bg-[#2A58A8]  text-white">TRAVEL</div>
+      <Link to="/" className="px-8 py-3 bg-[#2A58A8]  text-white">
+        TRAVEL
+      </Link>
       <button className="px-8 py-3">找房源</button>
     </div>
   );
