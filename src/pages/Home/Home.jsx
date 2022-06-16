85ce9ec01493295cbe7d2bc3cc13c97cd8514c6a
Link1515 <h26281660@gmail.com>
2022-06-16 16:43:40 +0800

    feat(home.jsx): 修改首頁樣式，改用 grid

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
   }
 
   return (
-    <div className="container flex flex-wrap">
+    <div className="container p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {roomList.map((roomData) => (
         <Card roomData={roomData} key={roomData.id} />
       ))}
