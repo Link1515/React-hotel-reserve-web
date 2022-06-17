87866093c327becf69e4f9313671650eea264f96
Link1515 <h26281660@gmail.com>
2022-06-17 09:07:03 +0800

    docs(api): 修改 api 文件變數命名

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 const axios = require('axios').default;
 
-export const roomAPI = axios.create({
+export const serverApi = axios.create({
   baseURL: 'https://challenge.thef2e.com/api/thef2e2019/stage6',
   headers: { authorization: 'Bearer ' + process.env.REACT_APP_API_KEY }
 });
--- src/api/room.js
+++ src/api/room.js
@@ -1,8 +1,8 @@
-import { roomAPI } from './config';
+import { serverApi } from './config';
 
 // 取得房間清單
 export const fetchRoomList = () => {
-  return roomAPI({
+  return serverApi({
     method: 'get',
     url: '/rooms'
   });
@@ -10,7 +10,7 @@ export const fetchRoomList = () => {
 
 // 取得房間內容
 export const fetchRoomDetail = (roomId) => {
-  return roomAPI({
+  return serverApi({
     method: 'get',
     url: `/room/${roomId}`
   });
@@ -18,7 +18,7 @@ export const fetchRoomDetail = (roomId) => {
 
 // 預約房間
 export const reserveRoom = (roomId, data) => {
-  return roomAPI({
+  return serverApi({
     method: 'post',
     url: `/rooms/${roomId}`,
     data
@@ -27,7 +27,7 @@ export const reserveRoom = (roomId, data) => {
 
 // 清除所有預約
 export const unreserveAllRooms = () => {
-  return roomAPI({
+  return serverApi({
     method: 'delete',
     url: `/rooms`
   });
