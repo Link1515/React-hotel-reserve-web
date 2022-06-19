d44ab01345ed87756f1e786763c39317295a813e
Link1515 <h26281660@gmail.com>
2022-06-19 21:23:36 +0800

    feat(app.js): 加入 NotFound page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 // pages
 import Home from './pages/Home';
 import RoomDetail from './pages/RoomDetail';
+import NotFound from './pages/NotFound';
 // components
 import Header from './components/Header';
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/room/:roomId" element={<RoomDetail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
