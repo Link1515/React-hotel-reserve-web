af8b069320c788c4d5424d912ba3cf6a963638d4
Link1515 <h26281660@gmail.com>
2022-06-19 21:13:58 +0800

    feat(header.jsx): header 取消找房源按鈕

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,10 +3,9 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <div className="flex justify-between bg-white">
-      <Link to="/" className="px-8 py-3 bg-[#2A58A8]  text-white">
+      <Link to="/" className="px-8 py-3 bg-[#2A58A8] block text-white">
         TRAVEL
       </Link>
-      <button className="px-8 py-3">找房源</button>
     </div>
   );
 }
